1ee57b9c523cdc59fe869448fce0a9303a9944b2
ILKERC <35669865+ilkerccom@users.noreply.github.com>
2023-06-09 15:42:08 +0300

    Update Program.cs
    - If private key found, it will no longer be flagged on database. (For security)
    - Some changes have been applied to make sure the scan is done.
    - Telegram support

--- BitcrackRandomiser/Program.cs
+++ BitcrackRandomiser/Program.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Threading;
 using System.Runtime;
+using System.Text;
 
 namespace BitcrackRandomiser
 {
@@ -28,7 +29,12 @@ namespace BitcrackRandomiser
         static Task<int> RunBitcrack()
         {
             // Get scan type. Include or exclude defeated ranges
-            string ScanType = Helpers.GetSettings(3);
+            string ScanType = Helpers.GetSettings(3).Split('=')[1].ToLower();
+            string TelegramShare = Helpers.GetSettings(5).Split('=')[1].ToLower();
+            string TelegramShareEachKey = Helpers.GetSettings(8).Split('=')[1].ToLower();
+
+            // Is scan finished?
+            bool IsFinished = false;
 
             // Get random HEX value 
             string RandomHex = Requests.GetHex(ScanType).Result;
@@ -50,7 +56,7 @@ namespace BitcrackRandomiser
             // Convert numbers to HEX
             string StartHex = RandomHex;
             string EndHex = EndNumber.ToString("X");
-            string WalletAddress = Helpers.GetSettings(2);
+            string WalletAddress = Helpers.GetSettings(2).Split('=')[1];
 
             // Testing
             bool isTesting = false;
@@ -66,7 +72,7 @@ namespace BitcrackRandomiser
             Helpers.WriteLine(string.Format("HEX range: {0}-{1}", StartHex, EndHex));
             Helpers.WriteLine(string.Format("Target address: {0}", TargetAddress));
             Helpers.WriteLine(string.Format("Progress: {0}", "Visit the <btcpuzzle.info> for statistics."));
-            Helpers.WriteLine(string.Format("Your wallet address: {0}", Helpers.GetSettings(2)));
+            Helpers.WriteLine(string.Format("Your wallet address: {0}", WalletAddress));
 
             // Get settings and arguments from setting.txt
             string BitcrackFolder = Helpers.GetSettings(0);
@@ -78,37 +84,82 @@ namespace BitcrackRandomiser
             // Proccess info
             var process = new Process
             {
-                StartInfo = { FileName = BitcrackFolder, Arguments = BitcrackArguments },
+                StartInfo = { FileName = BitcrackFolder, RedirectStandardError = true, RedirectStandardOutput = true, Arguments = BitcrackArguments },
                 EnableRaisingEvents = true
+            };
+
+            // Output from BitCrack
+            process.ErrorDataReceived += (object o, DataReceivedEventArgs e) =>
+            {
+                if (Helpers.CheckJobIsFinished(o, e))
+                {
+                    IsFinished = true;
+                }
+            };
+
+            // Output from BitCrack
+            process.OutputDataReceived += (object o, DataReceivedEventArgs e) =>
+            {
+                if (Helpers.CheckJobIsFinished(o, e))
+                {
+                    IsFinished = true;
+                }
             };
 
             // Bitcrack exited
             process.Exited += (sender, args) =>
             {
-                if(process.ExitCode == 0)
+                if(process.ExitCode == 0 && IsFinished)
                 {
                     int FlagTries = 1;
 
-                    // Flag HEX as used
-                    bool FlagUsed = Requests.SetHex(StartHex, WalletAddress).Result;
+                    // Check winner
+                    Found = Helpers.CheckWinner(TargetAddress, StartHex);
 
-                    // Try flagging
-                    while (!FlagUsed && FlagTries <= 3)
+                    // Run again if not found
+                    if (Found)
                     {
-                        FlagUsed = Requests.SetHex(StartHex, WalletAddress).Result;
-                        Helpers.WriteLine(string.Format("Flag error... Retrying... {0}/3", FlagTries));
-                        Thread.Sleep(5000);
-                        FlagTries++;
-                    }
+                        if (TelegramShare == "true")
+                        {
+                            Helpers.ShareTelegram(string.Format("Congratulations. Private Key Found in HEX range = {0}. Please check your folder or console screen to get key", StartHex));
+                        }
 
-                    // Info
-                    if (FlagUsed)
-                    {
-                        Helpers.WriteLine("Range scanned and flagged successfully... Launching again... Please wait...");
+                        Helpers.WriteLine("Congratulations. Key found. Please check your folder.");
+                        Helpers.WriteLine("You can donate me; 1eosEvvesKV6C2ka4RDNZhmepm1TLFBtw");
+                        Console.ReadLine();
                     }
                     else
                     {
-                        Helpers.WriteLine("Range scanned with flag error... Launching again... Please wait...");
+                        if (TelegramShare == "true" && TelegramShareEachKey == "true")
+                        {
+                            Helpers.ShareTelegram(string.Format("HEX {0} scanned in worker {1}", StartHex, WalletAddress));
+                        }
+
+                        // Flag HEX as used
+                        bool FlagUsed = Requests.SetHex(StartHex, WalletAddress).Result;
+
+                        // Try flagging
+                        while (!FlagUsed && FlagTries <= 3)
+                        {
+                            FlagUsed = Requests.SetHex(StartHex, WalletAddress).Result;
+                            Helpers.WriteLine(string.Format("Flag error... Retrying... {0}/3", FlagTries));
+                            Thread.Sleep(5000);
+                            FlagTries++;
+                        }
+
+                        // Info
+                        if (FlagUsed)
+                        {
+                            Helpers.WriteLine("Range scanned and flagged successfully... Launching again... Please wait...");
+                        }
+                        else
+                        {
+                            Helpers.WriteLine("Range scanned with flag error... Launching again... Please wait...");
+                        }
+
+                        // Wait and restart
+                        Thread.Sleep(10000);
+                        RunBitcrack();
                     }
                 }
                 else
@@ -117,28 +168,14 @@ namespace BitcrackRandomiser
                     Helpers.WriteLine("Bitcrack app exited with unknown code... Launching again... Please wait...");
                 }
 
-                // Check winner
-                Found = Helpers.CheckWinner(TargetAddress, StartHex);
-
-                // Run again
-                if (!Found)
-                {
-                    Thread.Sleep(10000);
-                    RunBitcrack();
-                }
-                else
-                {
-                    Helpers.WriteLine("Congratulations. Key found. Please check your folder.");
-                    Helpers.WriteLine("You can donate me; 1eosEvvesKV6C2ka4RDNZhmepm1TLFBtw");
-                    Console.ReadLine();
-                }
-
                 taskCompletionSource.SetResult(process.ExitCode);
                 process.Dispose();
             };
 
             // Start bitcrack app
             process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
             return taskCompletionSource.Task;
         }
     }
