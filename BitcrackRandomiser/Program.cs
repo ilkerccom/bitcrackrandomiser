83ca8196e78b4aa00d215e61c91e15f9571d8193
ilkerc <you@example.com>
2023-06-30 04:41:28 +0300

    Revert "Console problem"
    This reverts commit 55a6a89243e8f9cf5f1a3c9404a1b128af193b03.

--- BitcrackRandomiser/Program.cs
+++ BitcrackRandomiser/Program.cs
@@ -28,7 +28,7 @@ namespace BitcrackRandomiser
 
             // Edit settings
             Helpers.WriteLine(string.Format("Press any key to edit settings or wait for {0} seconds to load app with <settings.txt>", 3));
-            bool EditSettings = Task.Factory.StartNew(() => { try { Console.ReadKey(); } catch { } }).Wait(TimeSpan.FromSeconds(3));
+            bool EditSettings = Task.Factory.StartNew(() => Console.ReadKey()).Wait(TimeSpan.FromSeconds(3));
             if (EditSettings)
             {
                 AppSettings = Settings.SetSettings();
